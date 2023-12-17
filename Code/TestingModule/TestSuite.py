48e8a2d904007903cb236910d6115c3a552793a8
Navaira Rehman <nrehman.bese21seecs@student.nust.edu.pk>
2023-12-17 15:31:33 +0500

    Add Test Cases for ViewCustomerWindow

--- Code/TestingModule/TestSuite.py
+++ Code/TestingModule/TestSuite.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from AddInstallmentWindow import AddInstallmentWindow
 from ClientFromWindow import ClientFromWindow
+from ViewCustomerWindow import ViewCustomerWindow
 
 # Testing AddInstallementWindow File
 class TestAddInstallement(unittest.TestCase):
@@ -67,6 +68,63 @@ class TestClientForm(unittest.TestCase):
     def testCustomerDetials(self):
         customerDetials = ClientFromWindow.getCustomerByCNIC(self, self.customerCNIC)
         self.assertEqual(customerDetials, self.customerDetials)
+
+# Testing ViewCustomerWindow File
+class TestViewCustomerWindow(unittest.TestCase):
+    """
+    Test case class for testing the methods of the ViewCustomerWindow class.
+
+    Testing Strategy:
+    - Test the getTotalNoOfAccounts method to ensure it retrieves the correct total number of accounts.
+    - Test the getNoOfActiveAccounts method to ensure it retrieves the correct number of active accounts.
+    - Test the getAllAccounts method to ensure it retrieves all accounts correctly.
+    - Test the getCustomersOnFilter method for "Dead" status to ensure it filters dead accounts correctly.
+    - Test the getCustomersOnFilter method for "Active" status to ensure it filters active accounts correctly.
+
+    Test Cases:
+    - testGetTotalNoOfAccounts: Test the getTotalNoOfAccounts method.
+    - testGetNoOfActiveAccounts: Test the getNoOfActiveAccounts method.
+    - testGetAllAccounts: Test the getAllAccounts method.
+    - testGetCustomersOnDeadFilter: Test the getCustomersOnFilter method for "Dead" status.
+    - testGetCustomersOnActiveFilter: Test the getCustomersOnFilter method for "Active" status.
+    """
+    def setUp(self):
+         # Test data for the ViewCustomerWindow class
+        self.noOfTotalAccounts = 6
+        self.noOfActiveAccounts = 5
+        self.allAccounts = [(1, 100, 'Vishal Sagar', 'Fan', '03220988774', 30000, 'M', 'Dead', 'NUST'),
+                            (2, 100, 'Vishal Sagar', 'Air Cooler', '03220988774', 30000, 'M', 'Active', 'NUST'), 
+                            (3, 101, 'Abdul  Basit', 'Air Cooler', '03238977665', 90000, 'F', 'Active', 'NUST'), 
+                            (4, 102, 'Ahmed Abdullah', 'MotorBike', '03238977665', 200000, 'M', 'Active', 'NUST'), 
+                            (5, 103, 'Nawaz Sharif', 'Fan', '0323897766543', 90000, 'M', 'Active', 'England'), 
+                            (6, 104, 'Ali Akbar', 'Fan', '03485998674', 50000, 'M', 'Active', 'NUST')]
+        self.deadAccounts = [(1, 100, 'Vishal Sagar', 'Fan', '03220988774', 30000, 'M', 'Dead', 'NUST')]
+        self.activeAccounts = [(2, 100, 'Vishal Sagar', 'Air Cooler', '03220988774', 30000, 'M', 'Active', 'NUST'), 
+                               (3, 101, 'Abdul  Basit', 'Air Cooler', '03238977665', 90000, 'F', 'Active', 'NUST'), 
+                               (4, 102, 'Ahmed Abdullah', 'MotorBike', '03238977665', 200000, 'M', 'Active', 'NUST'), 
+                               (5, 103, 'Nawaz Sharif', 'Fan', '0323897766543', 90000, 'M', 'Active', 'England'),
+                               (6, 104, 'Ali Akbar', 'Fan', '03485998674', 50000, 'M', 'Active', 'NUST')]
+
+    def testGetTotalNoOfAccounts(self):
+        noOfTotalAccounts = ViewCustomerWindow.getTotalNoOfAccounts(self)
+        self.assertEqual(noOfTotalAccounts, self.noOfTotalAccounts)
+
+    def testGetNoOfActiveAccounts(self):
+        noOfActiveAccounts = ViewCustomerWindow.getNoOfActiveAccounts(self)
+        self.assertEqual(noOfActiveAccounts, self.noOfActiveAccounts)
+    
+    def testGetAllAccounts(self):
+        allAccounts = ViewCustomerWindow.getAllAccounts(self)
+        self.assertEqual(allAccounts, self.allAccounts)
+
+    def testGetCustomersOnDeadFilter(self):
+        deadAccounts = ViewCustomerWindow.getCustomersOnFilter(self, "Dead")
+        self.assertEqual(deadAccounts, self.deadAccounts)
+    
+    def testGetCustomersOnActiveFilter(self):
+        activeAccounts = ViewCustomerWindow.getCustomersOnFilter(self, "Active")
+        self.assertEqual(activeAccounts, self.activeAccounts)
+
 
 if __name__ == "__main__":
     unittest.main()
